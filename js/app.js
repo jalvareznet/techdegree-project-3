3e3f8ee53b89081e8f187ffd8ad762a7feb98797
Juan Álvarez <juan@jalvarez.net>
2018-06-06 18:55:28 +0200

    question t-shirt info

--- js/app.js
+++ js/app.js
@@ -9,6 +9,11 @@ fieldset.addEventListener('change', (e) => {
   const otherValue = document.querySelector('option[value="other"]');
   const other = otherValue.value;
 
+//if inputField already exists remove it. This is for remove when the option change.
+  if( inputField = document.querySelector('#other-title')){
+  fieldset.removeChild(inputField);
+}
+//if selected is equal to other then create and append the new field.
   if (selected === other ) {
       //2.1.- create input element with text type.
       const inputField = document.createElement('input');
@@ -23,39 +28,43 @@ fieldset.addEventListener('change', (e) => {
       //2.3.- append input to the fieldset.
       fieldset.appendChild(inputField);
     }
-    if( selected !== other ){
-      const inputField = document.querySelector('#other-title')
-    fieldset.removeChild(inputField);
-  }
 });
 
-// //3.1 only display the color options that match the design selected.
-//
-// const shirtFieldset = document.querySelector('.shirt');
-//
-// shirtFieldset.addEventListener('click', (e) => {
-//
-//   const selected = e.target.value;
-//
-//   const jsPunsValue = document.querySelector('option[value="js puns"]');
-//   const jsPuns = jsPunsValue.value;
-//
-//   const heartJsValue = document.querySelector('option[value="heart js"]');
-//   const heartJs = heartJsValue.value;
-//
-//   if ( selected === jsPuns ) {
-//     const tomato = document.querySelector('option[value="tomato"]');
-//     tomato.style.display = 'none';
-//     const steelblue = document.querySelector('option[value="steelblue"]');
-//     steelblue.style.display = 'none';
-//     const dimgrey = document.querySelector('option[value="dimgrey"]');
-//     dimgrey.style.display = 'none';
-//   } else if ( selected === heartJs ) {
-//       const cornflowerblue = document.querySelector('option[value="cornflowerblue"]');
-//       cornflowerblue.style.display = 'none';
-//       const darkslategrey = document.querySelector('option[value="darkslategrey"]');
-//       darkslategrey.style.display = 'none';
-//       const gold = document.querySelector('option[value="gold"]');
-//       gold.style.display = 'none';
-//       }
-// });
+//question 1 --
+//3.1 only display the color options that match the design selected.
+
+  const shirtFieldset = document.querySelector('.shirt');
+  const tomato = document.querySelector('option[value="tomato"]');
+  const steelblue = document.querySelector('option[value="steelblue"]');
+  const dimgrey = document.querySelector('option[value="dimgrey"]');
+  const cornflowerblue = document.querySelector('option[value="cornflowerblue"]');
+  const darkslategrey = document.querySelector('option[value="darkslategrey"]');
+  const gold = document.querySelector('option[value="gold"]');
+
+shirtFieldset.addEventListener('click', (e) => {
+
+  const selected = e.target.value;
+
+  const jsPunsValue = document.querySelector('option[value="js puns"]');
+  const jsPuns = jsPunsValue.value;
+
+  const heartJsValue = document.querySelector('option[value="heart js"]');
+  const heartJs = heartJsValue.value;
+
+  if ( selected === jsPuns ) {
+    tomato.style.display = 'none';
+    steelblue.style.display = 'none';
+    dimgrey.style.display = 'none';
+    cornflowerblue.style.display = 'block';
+    darkslategrey.style.display = 'block';
+    gold.style.display = 'block';
+    }
+  if ( selected === heartJs ) {
+    tomato.style.display = 'block';
+    steelblue.style.display = 'block';
+    dimgrey.style.display = 'block';
+    cornflowerblue.style.display = 'none';
+    darkslategrey.style.display = 'none';
+    gold.style.display = 'none';
+    }
+});
