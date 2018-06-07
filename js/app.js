0817a0d03f15e9f71cb70e5bac1f0a68679663b7
Juan Álvarez <juan@jalvarez.net>
2018-06-07 19:00:14 +0200

    second upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //1.- When the page loads, give focus to the first text field
-document.getElementById('name').focus();
-
+const inputName = document.getElementById('name');
+inputName.focus();
+const inputMail = document.getElementById('mail');
 //2.- If job role is in other --> text field is revealed.
 const fieldset = document.querySelector('fieldset');
 
@@ -30,7 +31,6 @@ fieldset.addEventListener('change', (e) => {
     }
 });
 
-//question 1 --
 //3.1 only display the color options that match the design selected.
 
   const shirtFieldset = document.querySelector('.shirt');
@@ -41,7 +41,10 @@ fieldset.addEventListener('change', (e) => {
   const darkslategrey = document.querySelector('option[value="darkslategrey"]');
   const gold = document.querySelector('option[value="gold"]');
 
-shirtFieldset.addEventListener('click', (e) => {
+  const divColors = document.querySelector('#colors-js-puns');
+  divColors.style.display = 'none';
+
+shirtFieldset.addEventListener('change', (e) => {
 
   const selected = e.target.value;
 
@@ -51,7 +54,12 @@ shirtFieldset.addEventListener('click', (e) => {
   const heartJsValue = document.querySelector('option[value="heart js"]');
   const heartJs = heartJsValue.value;
 
+  const selectedAttr = document.createAttribute('selected');
+
   if ( selected === jsPuns ) {
+    divColors.style.display = 'block';
+    tomato.removeAttribute('selected');
+    cornflowerblue.setAttributeNode(selectedAttr);
     tomato.style.display = 'none';
     steelblue.style.display = 'none';
     dimgrey.style.display = 'none';
@@ -60,6 +68,9 @@ shirtFieldset.addEventListener('click', (e) => {
     gold.style.display = 'block';
     }
   if ( selected === heartJs ) {
+    divColors.style.display = 'block';
+    cornflowerblue.removeAttribute('selected');
+    tomato.setAttributeNode(selectedAttr);
     tomato.style.display = 'block';
     steelblue.style.display = 'block';
     dimgrey.style.display = 'block';
@@ -67,4 +78,200 @@ shirtFieldset.addEventListener('click', (e) => {
     darkslategrey.style.display = 'none';
     gold.style.display = 'none';
     }
+  if ( selected === 'Select Theme') {
+    divColors.style.display = 'none';
+  }
 });
+
+//3.1 ”Register for Activities” section of the form:
+const fieldsetActivities = document.querySelector('.activities');
+
+const all = document.querySelector('input[name="all"]');
+const frameworks = document.querySelector('input[name="js-frameworks"]');
+const libs = document.querySelector('input[name="js-libs"]');
+const express = document.querySelector('input[name="express"]');
+const node = document.querySelector('input[name="node"]');
+const buildTools = document.querySelector('input[name="build-tools"]');
+const npm = document.querySelector('input[name="npm"]');
+
+const disabledAttr = document.createAttribute('disabled');
+
+const allPrice = document.createAttribute('price')
+const frameworksPrice = document.createAttribute('price')
+const libsPrice = document.createAttribute('price')
+const expressPrice = document.createAttribute('price')
+const nodePrice = document.createAttribute('price')
+const buildToolsPrice = document.createAttribute('price')
+const npmPrice = document.createAttribute('price')
+
+const totalPriceDiv = document.createElement('div');
+
+fieldsetActivities.addEventListener('change', () => {
+
+if (all.checked) {
+  allPrice.value = 200;
+} else {
+  allPrice.value = 0;
+}
+if ( frameworks.checked ) {
+  express.disabled = true;
+  frameworksPrice.value = 100;
+  if (true) {
+    express.parentNode.style.color = 'grey';
+  }
+} else {
+  express.disabled = false;
+  express.parentNode.style.color = '';
+  frameworksPrice.value = 0;
+}
+if ( express.checked ) {
+  frameworks.disabled = true;
+  expressPrice.value = 100;
+  if (true) {
+    frameworks.parentNode.style.color = 'grey';
+  }
+} else {
+  frameworks.disabled = false;
+  frameworks.parentNode.style.color = '';
+  expressPrice.value = 0;
+}
+if ( libs.checked ) {
+  node.disabled = true;
+  libsPrice.value = 100;
+  if (true) {
+    node.parentNode.style.color = 'grey';
+  }
+} else {
+  node.disabled = false;
+  node.parentNode.style.color = '';
+  libsPrice.value = 0;
+}
+if ( node.checked ) {
+  libs.disabled = true;
+  nodePrice.value = 100;
+  if (true) {
+    libs.parentNode.style.color = 'grey';
+  }
+} else {
+  libs.disabled = false;
+  libs.parentNode.style.color = '';
+  nodePrice.value = 0;
+}
+
+if (buildTools.checked) {
+  buildToolsPrice.value = 100;
+} else {
+  buildToolsPrice.value = 0;
+}
+
+if (npm.checked) {
+  npmPrice.value = 100;
+} else {
+  npmPrice.value = 0;
+}
+
+//3.3 total amount $ in courses
+all.setAttributeNode(allPrice);
+frameworks.setAttributeNode(frameworksPrice);
+libs.setAttributeNode(libsPrice);
+express.setAttributeNode(expressPrice);
+node.setAttributeNode(nodePrice);
+buildTools.setAttributeNode(buildToolsPrice);
+npm.setAttributeNode(npmPrice);
+
+const total =
++all.getAttribute('price') +
++frameworks.getAttribute('price') +
++libs.getAttribute('price') +
++express.getAttribute('price') +
++node.getAttribute('price') +
++buildTools.getAttribute('price') +
++npm.getAttribute('price');
+
+
+totalPriceDiv.innerHTML = "<h3> Total: $" + total + "<h3>";
+fieldsetActivities.appendChild(totalPriceDiv);
+
+if ( total === 0 ) {
+  fieldsetActivities.removeChild(totalPriceDiv);
+}
+});
+
+
+//4.- Payment Info section of the form:
+
+const divCreditCard = document.querySelector('#credit-card');
+divCreditCard.style.display = 'block';
+
+const divPaypal = divCreditCard.nextElementSibling;
+divPaypal.style.display = 'none';
+
+const divBitcoin = divPaypal.nextElementSibling;
+divBitcoin.style.display = 'none';
+
+const lastFieldset = divCreditCard.parentNode;
+
+const creditCard = document.querySelector('[value="credit card"]');
+const selectedAttr = document.createAttribute('selected');
+creditCard.setAttributeNode(selectedAttr);
+
+
+lastFieldset.addEventListener('change', (e) => {
+  const selected = e.target.value;
+
+  if ( selected === 'credit card' ) {
+    divCreditCard.style.display = 'block';
+    divPaypal.style.display = 'none';
+    divBitcoin.style.display = 'none';
+  }
+  if ( selected === 'paypal') {
+    divPaypal.style.display = 'block';
+    divBitcoin.style.display = 'none';
+    divCreditCard.style.display = 'none';
+  }
+  if ( selected === 'bitcoin') {
+    divBitcoin.style.display = 'block';
+    divPaypal.style.display = 'none';
+    divCreditCard.style.display = 'none';
+  }
+  if ( selected === 'select_method') {
+    divBitcoin.style.display = 'none';
+    divPaypal.style.display = 'none';
+    divCreditCard.style.display = 'none';
+  }
+});
+
+
+const requiredAttrName = document.createAttribute('required');
+const requiredAttrMail = document.createAttribute('required');
+const requiredNumberCreditCard = document.createAttribute('required minlength="6" ');
+inputName.setAttributeNode(requiredAttrName);
+inputMail.setAttributeNode(requiredAttrMail);
+
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
